806c92d5edac31b1bbbd0d233684c49b94dd7fb4
Rafaela-Menezes <rafaelamenezes.ti@gamil.com>
2025-12-05 22:24:24 -0300

    Circulo comentado

--- Circulo.java
+++ Circulo.java
@@ -1,7 +1,9 @@
 public class Circulo {
     private int posicaoX;
     private int posicaoY;
+    // define o diâmetro de todos os círculos
     public static final int DIAMETRO = 70;
+    // construtor para definir a posição de cada círculo
     public Circulo(int posicaoX, int posicaoY){
         this.posicaoX=posicaoX;
         this.posicaoY=posicaoY;
